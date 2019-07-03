dd9548f3c1b7c2f976bbe7bdf837da590b4263c0
Lukas Bombach <lukas.bombach@googlemail.com>
2019-07-03 21:03:53 +0200

    able to turn on/off and connect to de1 using hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,79 @@
-import React, { useState } from "react";
-import DE1 from "de1";
+import React, { useState, useEffect } from "react";
+import de1 from "de1";
 import "./App.css";
 
-const de1 = new DE1();
+const { REACT_APP_HUE_API, REACT_APP_HUE_SECRET } = process.env;
+const switchApi = `${REACT_APP_HUE_API}${REACT_APP_HUE_SECRET}/lights/12`;
 
-function useDe1Connection(): [boolean, () => Promise<void>] {
+function useDe1Connection(): [boolean, boolean, () => Promise<void>] {
   const [isConnected, setIsConnected] = useState(false);
+  const [isConnecting, setIsConnecting] = useState(false);
   const connect = async () => {
-    await de1.connect();
-    const newIsConnected = await de1.isConnected();
-    setIsConnected(newIsConnected);
+    try {
+      setIsConnecting(true);
+      await de1.connect();
+      setIsConnecting(false);
+      const newIsConnected = await de1.isConnected();
+      setIsConnected(newIsConnected);
+    } catch (error) {
+      console.error(error);
+    }
   };
-  return [isConnected, connect];
+  return [isConnected, isConnecting, connect];
 }
 
-// function useDe1State() {}
+function useCoffeeSwitch(
+  initialValue = false
+): [boolean, (on: boolean) => Promise<void>] {
+  const [isSwitchOn, setIsSwitchOn] = useState(initialValue);
+
+  const getSwitchState = async () => {
+    try {
+      const response = await fetch(switchApi);
+      const { state } = await response.json();
+      setIsSwitchOn(state.on);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const setSwitchState = async (on: boolean) => {
+    try {
+      await fetch(`${switchApi}/state`, {
+        method: "PUT",
+        body: JSON.stringify({ on })
+      });
+      getSwitchState();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getSwitchState();
+  }, []);
+
+  return [isSwitchOn, setSwitchState];
+}
 
 const App: React.FC = () => {
-  const [isConnected, connect] = useDe1Connection();
-
-  console.log("Is DE1 currently connected?", isConnected);
+  const [isConnected, isConnecting, connect] = useDe1Connection();
+  const [isSwitchOn, setSwitchOn] = useCoffeeSwitch();
 
   return (
     <div className="App">
       <header className="App-header">
+        <p>{isSwitchOn ? "on" : "off"}</p>
         <p>{isConnected ? "connected" : "disconnected"}</p>
+        <button onClick={() => setSwitchOn(!isSwitchOn)}>
+          {!isSwitchOn ? "On" : "Off"}
+        </button>
         <button
-          disabled={isConnected}
-          onClick={async () => {
-            try {
-              await connect();
-            } catch (error) {
-              console.error(error);
-            }
-          }}
+          disabled={isConnected || isConnecting}
+          onClick={() => connect()}
         >
-          {isConnected ? "Connected" : "Connect to DE1"}
-        </button>{" "}
+          {!isConnected ? "Connect" : "Connected"}
+        </button>
       </header>
     </div>
   );
